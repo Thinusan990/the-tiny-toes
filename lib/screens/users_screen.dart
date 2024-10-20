ad6babc51a4a86a5ce3c973e831a398591c36e2d
Thinusan <Thinusan@example.com>
2024-10-20 19:21:27 +0530

    #7 user page created which will fetch the users list from the api and connected with the provider

---
+++ lib/screens/users_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:the_tiny_toes/server/network_service.dart';
+
+class UsersScreen extends StatefulWidget {
+  @override
+  _UsersScreenState createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  NetworkService _networkService = NetworkService();
+  List<dynamic> _users = [];
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUsers();
+  }
+
+  void _fetchUsers() async {
+    try {
+      var users = await _networkService.fetchUsers();
+      setState(() {
+        _users = users;
+        _loading = false;
+      });
+    } catch (e) {
+      // Handle error state
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Users'),
+      ),
+      body: _loading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: _users.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_users[index]['name']),
+            subtitle: Text(_users[index]['email']),
+            onTap: () {
+              Navigator.pushNamed(context, '/albums', arguments: _users[index]['id']);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
