a2f59e37b037ad4e72d90de3e7388c944b9e20b3
Thinusan <Thinusan@example.com>
2024-10-20 19:25:51 +0530

    #9 gallery page created which will fetch the photos  which are from the album from the api and connected with the provider

---
+++ lib/screens/gallery_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:the_tiny_toes/server/network_service.dart';
+
+class GalleryScreen extends StatefulWidget {
+  final int albumId;
+
+  GalleryScreen({required this.albumId});
+
+  @override
+  _GalleryScreenState createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  NetworkService _networkService = NetworkService();
+  List<dynamic> _photos = [];
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPhotos();
+  }
+
+  void _fetchPhotos() async {
+    try {
+      var photos = await _networkService.fetchPhotos(widget.albumId);
+      setState(() {
+        _photos = photos;
+        _loading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _loading = false;
+      });
+      _showErrorDialog(e.toString());
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(onPressed: () => Navigator.of(ctx).pop(), child: Text('OK')),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+      ),
+      body: _loading
+          ? Center(child: CircularProgressIndicator())
+          : GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, // Adjust the number of columns as necessary
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemCount: _photos.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/viewPhoto',
+                arguments: _photos[index],
+              );
+            },
+            child: Image.network(
+              _photos[index]['thumbnailUrl'],
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
